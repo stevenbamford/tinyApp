12a239b3fdee424a4572a4819237bdd8fb37dd5c
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-17 21:59:34 +0000

    Initial commit. Server is running. index shows full and shortened urls, with a placeholder link to create new url

---
+++ express_server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (request, response) => {
+  var templateVars = { urls: urlDatabase};
+  response.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (request, response) => {
+  var templateVars = { shortURL: request.params.id };
+  response.render("urls_show", templateVars);
+});
+
+app.get("/", (request, response) => {
+  response.end("Hello!");
+});
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
+});
+
+app.get("/hello", (request, response) => {
+  response.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});

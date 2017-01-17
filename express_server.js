2f1f9b8dbc8328555fb786b988222dac24be695f
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-17 23:07:59 +0000

    .ejs template files were implemented and are working

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -15,8 +18,27 @@ app.get("/urls", (request, response) => {
 });
 
 app.get("/urls/:id", (request, response) => {
-  var templateVars = { shortURL: request.params.id };
-  response.render("urls_show", templateVars);
+  if(urlDatabase.hasOwnProperty(request.params.id)){
+    var templateVars = {
+      shortURL: "http://" + request.params.id +".com",
+      longURL: urlDatabase[request.params.id]
+    }
+    response.render("urls_show", templateVars);
+  }else{
+    response.render("urls_show", {
+    shortURL: "URL not in database",
+    longURL: "URL not in database"
+    });
+  }
+});
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  respond.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (request, response) => {
